d8b7e25542cc7bf2160e2573b6820f31e3cad8fa
Manel Gasmi <manel.gasmi02@gmail.com>
2025-01-30 13:09:40 +0100

    add carousel to logement page

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./Carousel.scss";
+
+const Carousel = ({ logement }) => {
+  const [index, setIndex] = useState(0);
+
+  const nextImage = () => {
+    let newIndex = index + 1;
+    if (newIndex > logement.pictures.length - 1) {
+      newIndex = 0;
+    }
+    setIndex(newIndex);
+  };
+
+  const previousImage = () => {
+    let newIndex = index - 1;
+    if (newIndex < 0) {
+      newIndex = logement.pictures.length - 1;
+    }
+    setIndex(newIndex);
+  };
+  return (
+    <div className="carouselImageContainer">
+      <img className="carouselImage" src={logement.pictures[index]} />
+      {logement.pictures.length > 1 && (
+        <>
+          <div className="carouselCounter">
+            {index + 1}/{logement.pictures.length}
+          </div>
+          <i
+            className="fa-solid fa-chevron-left carouselPreviousButton "
+            onClick={previousImage}
+          ></i>
+          <i
+            className="fa-solid fa-chevron-right carouselNextButton"
+            onClick={nextImage}
+          ></i>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/logement/Logement.jsx
+++ src/pages/logement/Logement.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import Carousel from '../../components/carousel/Carousel';
 
 const Logement = () => {
   const location = useLocation();
   const { item } = location.state;
   return (
     <main className='container'>
+      <Carousel logement={item} />
       <h1>{item.title}</h1>
     </main>
   )
