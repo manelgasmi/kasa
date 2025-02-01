d988811fa4600d5f1de6223e59dfacac73e13f21
Manel Gasmi <manel.gasmi02@gmail.com>
2025-02-01 14:22:57 +0100

    page 404

--- src/pages/error404/Error404.jsx
+++ src/pages/error404/Error404.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./error404.scss";
+import { Link } from 'react-router-dom';
 
 export class Error404 extends Component {
   static propTypes = {}
 
   render() {
     return (
-      <div>Error404</div>
+      <div className='error-page'>
+        <h1>404</h1>
+        <p>Oups! La page demandée n'existe pas.</p>
+        <Link to ="/" className='back-home'>Retourner sur la page d'acceuil</Link>
+        </div>
     )
   }
 }
